8a2b58abcb12967c8134d21993d1280a82fe0c79
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2022-04-21 15:30:10 +0300

    Create top_k_frequent_ellements.py

---
+++ top_k_frequent_ellements.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        arr = []
+        nums.sort()
+        for num in nums:
+            if num not in arr:
+                arr.append(num)
+            else:
+                continue
+        ls =[]
+        print(arr)
+        for element in arr:
+            countr = nums.count(element)
+            ls.append([countr, element])
+        ls.sort(reverse=True)
+        answer = []
+        for i in range(k):
+            answer.append(ls[i][1])
+        return answer
