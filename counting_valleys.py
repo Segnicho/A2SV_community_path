5116f4f361b0c7643291e64e998717c20658c52a
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2022-04-21 21:46:37 +0300

    Update counting_valleys.py
    updating

--- counting_valleys.py
+++ counting_valleys.py
@@ -1,4 +1,21 @@
 #https://www.hackerrank.com/challenges/counting-valleys/problem
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
 def countingValleys(steps, path):
     mountain = valley = 0
     for word in path:
@@ -6,3 +23,16 @@ def countingValleys(steps, path):
         valley += 1 if mountain == 0 and word == 'U' else 0
     return valley
     
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
