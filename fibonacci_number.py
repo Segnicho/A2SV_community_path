9bb4dd86cb462109260caa66dce1c2e46d83159a
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2022-04-25 18:22:33 +0300

    Create fibonacci_number.py

---
+++ fibonacci_number.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/fibonacci-number/
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return n
+        else:
+            return  self.fib(n-2) + self.fib(n-1)
+   
