4f69b2fa4569349622d6f3873468beb9e9e4c5f3
Segni Desta <88204792+Segnicho@users.noreply.github.com>
2022-10-16 07:45:52 +0300

    Create best-time-to-buy-and-sell-stock-ii.py

---
+++ best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        proft = 0
+        currProfit = 0
+        for i in range(1,len(prices)):
+            if prices[i] > prices[i-1]:
+                currProfit = prices[i] - prices[i-1]
+                proft+=currProfit
+        return proft
